d1277186692ea4c5af9382acef692d0684ed3b29
dbarac <dario.barac1@gmail.com>
2022-10-16 13:13:18 +0200

    update

--- scripts/evaluate_ekf_configs.py
+++ scripts/evaluate_ekf_configs.py
@@ -1,4 +1,6 @@
 import argparse
+import logging
+import os
 import psutil
 import subprocess
 import signal
@@ -12,15 +14,16 @@ from rosbags.serde import deserialize_cdr
 
 def record_fused_odom(
     playback_bag_path: Union[Path, str], recording_bag_path: Union[Path, str],
-    playback_duration: Optional[int] = None, launch_wait_time: int = 5
+    playback_duration_sec: Optional[int] = None, launch_wait_time: int = 5
 ) -> None:
     """
     Record EKF-fused odometry generated while playing sensor data from
     the 'playback_bag_path' bag, and save it in 'recording_bag_path'.
     """
-    launch_proc = subprocess.Popen(["ros2", "launch", "sf_localization", "ekf.launch.py"])
-
-    # wait for nodes to initialize
+    launch_proc = subprocess.Popen(
+        ["ros2", "launch", "sensor_fusion_localization", "ekf_localization.launch.py"]
+    )
+    logging.info("Waiting for ROS nodes to initialize...")
     time.sleep(launch_wait_time)
 
     topics_to_record = [
@@ -29,14 +32,17 @@ def record_fused_odom(
     bag_recording_process = subprocess.Popen(
         ["ros2", "bag", "record"] + topics_to_record + ["-o", recording_bag_path]
     )
+    logging.info("Playing sensor data rosbag & recording fused output...")
     bag_playback_process = subprocess.Popen(["ros2", "bag", "play", playback_bag_path])
 
-    # stop recording & kill started nodes when bag playback ends
     try:
-        bag_playback_process.wait(timeout=playback_duration)
-    except TimeoutExpired:
+        logging.info("Waiting for bag playback to complete...")
+        bag_playback_process.wait(timeout=playback_duration_sec)
+    except subprocess.TimeoutExpired:
         bag_playback_process.send_signal(signal.SIGINT) # stop playback
+    logging.info("Done.")
 
+    # stop recording
     bag_recording_process.send_signal(signal.SIGINT)
 
     # sending SIGINT to launch_proc should be enough to kill started nodes
@@ -48,32 +54,48 @@ def record_fused_odom(
 
 
 def odom_msg_get_pos(raw_msg: bytes) -> (float, float):
-    msg = deserialize_cdr(rawdata, connection.msgtype)
+    msg = deserialize_cdr(raw_msg, "nav_msgs/msg/Odometry")
     pos = msg.pose.pose.position
     return pos.x, pos.y
 
-def plot_robot_odometry(rosbag_path: Union[Path, str]):
+
+def plot_robot_odometry(rosbag_path: Union[Path, str]) -> None:
     positions = {
         "odom": [], "fused": []
     }
     with Reader(rosbag_path) as reader:
-        for connection, timestamp, rawdata in reader.messages():
+        for connection, timestamp, raw_data in reader.messages():
             if connection.topic == "/husky_velocity_controller/odom":
-                positions["odom"].append(odom_msg_get_pos(rawdata))
+                positions["odom"].append(odom_msg_get_pos(raw_data))
             elif connection.topic == "/odometry/filtered":
-                positions["fused"].append(odom_msg_get_pos(rawdata))
+                positions["fused"].append(odom_msg_get_pos(raw_data))
+                print(positions["fused"][-1])
 
     x, y = zip(*positions["odom"])
-    plt.plot(x, y)
-    x, y = zip(*positions["fused"])
+    #logging.info("LEN odom: {}", len(x))
+    #plt.figure()
+    plt.plot(x, y, label="odom")
+    #plt.show(block=False)
+    x2, y2 = zip(*positions["fused"])
+    #logging.info("LEN fused: {}", % len(x2))
+    #plt.figure()
+    plt.plot(x2, y2, label="fused")
+    #plt.show(block=False)
+    plt.legend(loc="lower left")
+    plt.title("Robot odometry in /odom frame")
     plt.show()
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     ap = argparse.ArgumentParser()
     ap.add_argument(
         "-s", "--sensor_bag", type=str,
         help="path to ros2 bag with recorded sensor data"
+    )
+    ap.add_argument(
+        "-c", "--base_config", type=str,
+        help="path to base (common) robot_localization .yaml config file"
     )
     ap.add_argument(
         "-d", "--output_dir", type=str, default="/tmp/ekf_eval/",
@@ -84,11 +106,10 @@ def main():
     assert args["sensor_bag"] is not None
     assert os.path.exists(args["output_dir"])
 
-    fusion_output_bag = os.path.join(args["output_dir"], "bags/test1")
-    print(fusion_output_bag)
+    fusion_output_bag = os.path.join(args["output_dir"], "bags/test1_fused")
 
     record_fused_odom(
-        args["sensor_bag"], fusion_output_bag, playback_duration=10
+        args["sensor_bag"], fusion_output_bag, playback_duration_sec=40
     )
     plot_robot_odometry(fusion_output_bag)
     # load + save ekf config 
