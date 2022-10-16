30d6b930dfa14a659ad0b28196e249a91bad678f
dbarac <dario.barac1@gmail.com>
2022-10-17 00:47:07 +0200

    update eval script

--- scripts/evaluate_ekf_configs.py
+++ scripts/evaluate_ekf_configs.py
@@ -5,20 +5,22 @@ import psutil
 import subprocess
 import signal
 import time
-from typing import Union, Optional
 from pathlib import Path
+from typing import List, Union, Optional
 
 import matplotlib.pyplot as plt
 from rosbags.rosbag2 import Reader
 from rosbags.serde import deserialize_cdr
 
-def record_fused_odom(
+def run_fusion_localization_on_sensor_data(
     playback_bag_path: Union[Path, str], recording_bag_path: Union[Path, str],
     playback_duration_sec: Optional[int] = None, launch_wait_time: int = 5
-) -> None:
+) -> List[str]:
     """
     Record EKF-fused odometry generated while playing sensor data from
     the 'playback_bag_path' bag, and save it in 'recording_bag_path'.
+
+    Returns list of recorded topics.
     """
     launch_proc = subprocess.Popen(
         ["ros2", "launch", "sensor_fusion_localization", "ekf_localization.launch.py"]
@@ -27,12 +29,13 @@ def record_fused_odom(
     time.sleep(launch_wait_time)
 
     topics_to_record = [
-        "/husky_velocity_controller/odom", "/fix", "/imu/data", "/odometry/filtered"
+        "/odom", "/odometry/gps", "/odometry/filtered"
     ]
     bag_recording_process = subprocess.Popen(
         ["ros2", "bag", "record"] + topics_to_record + ["-o", recording_bag_path]
     )
     logging.info("Playing sensor data rosbag & recording fused output...")
+
     bag_playback_process = subprocess.Popen(["ros2", "bag", "play", playback_bag_path])
 
     try:
@@ -40,7 +43,7 @@ def record_fused_odom(
         bag_playback_process.wait(timeout=playback_duration_sec)
     except subprocess.TimeoutExpired:
         bag_playback_process.send_signal(signal.SIGINT) # stop playback
-    logging.info("Done.")
+    logging.info("Bag playback completed.")
 
     # stop recording
     bag_recording_process.send_signal(signal.SIGINT)
@@ -52,6 +55,8 @@ def record_fused_odom(
     launch_proc.send_signal(signal.SIGINT)
     launch_proc.wait()
 
+    return topics_to_record
+
 
 def odom_msg_get_pos(raw_msg: bytes) -> (float, float):
     msg = deserialize_cdr(raw_msg, "nav_msgs/msg/Odometry")
@@ -59,35 +64,59 @@ def odom_msg_get_pos(raw_msg: bytes) -> (float, float):
     return pos.x, pos.y
 
 
-def plot_robot_odometry(rosbag_path: Union[Path, str]) -> None:
+def plot_robot_odometry(rosbag_path: Union[Path, str], odom_topics: List[str]) -> None:
     positions = {
-        "odom": [], "fused": []
+        topic: [] for topic in odom_topics
     }
     with Reader(rosbag_path) as reader:
         for connection, timestamp, raw_data in reader.messages():
-            if connection.topic == "/husky_velocity_controller/odom":
-                positions["odom"].append(odom_msg_get_pos(raw_data))
-            elif connection.topic == "/odometry/filtered":
-                positions["fused"].append(odom_msg_get_pos(raw_data))
-                print(positions["fused"][-1])
+            if connection.topic in odom_topics:
+                positions[connection.topic].append(odom_msg_get_pos(raw_data))
 
-    x, y = zip(*positions["odom"])
-    #logging.info("LEN odom: {}", len(x))
-    #plt.figure()
-    plt.plot(x, y, label="odom")
-    #plt.show(block=False)
-    x2, y2 = zip(*positions["fused"])
-    #logging.info("LEN fused: {}", % len(x2))
-    #plt.figure()
-    plt.plot(x2, y2, label="fused")
-    #plt.show(block=False)
+    for topic in positions.keys():
+        x, y = zip(*positions[topic])
+        plt.plot(x, y, label=topic)
+
     plt.legend(loc="lower left")
     plt.title("Robot odometry in /odom frame")
     plt.show()
 
 
+def evaluate_localization_configs(
+    base_rl_config: Union[Path, str], sensor_fusion_configs_yml: Union[Path, str],
+    playback_bag_path: Union[Path, str], output_dir: Union[Path, str]
+) -> None:
+    """Test and evaluate given robot_localization sensor configurations.
+
+    For each combination/configuration of sensors, defined as a top-level
+    entry in the sensor_fusion_configs_yml yaml file:
+      1. combine the given sensor configuration with the base ekf configuration
+         and save it
+      2. run sensor fusion localization launch file while playing sensor data
+         from given rosbag & and record fusion output
+      3. plot sensor odometry & fused odometry
+    """
+    with open(base_rl_config) as f:
+        common_config = yaml.safe_load(f)
+    with open(sensor_fusion_configs_yml) as f:
+        possible_fusion_configs = yaml.safe_load(f)
+
+    for config_name, sensor_config in possible_fusion_configs.items():
+        current_ekf_config = common_config.copy()
+        current_ekf_config.update(sensor_config)
+
+        with open('../config/ekf_current.yaml', 'w') as ekf_config_file:
+            yaml.dump(current_ekf_config, ekf_config_file)
+
+        fusion_output_bag_path = os.path.join(output_dir, "bags/", config_name, ".bag")
+        recorded_topics = run_fusion_localization_on_sensor_data(
+            playback_bag_path, fusion_output_bag_path
+        )
+        plot_robot_odometry(fusion_output_bag_path, recorded_topics)
+
 def main():
     logging.basicConfig(level=logging.INFO)
+
     ap = argparse.ArgumentParser()
     ap.add_argument(
         "-s", "--sensor_bag", type=str,
@@ -103,17 +132,17 @@ def main():
     )
     args = vars(ap.parse_args())
 
+    #assert os.path.exists(args["base_config"])
     assert args["sensor_bag"] is not None
     assert os.path.exists(args["output_dir"])
 
     fusion_output_bag = os.path.join(args["output_dir"], "bags/test1_fused")
 
-    record_fused_odom(
-        args["sensor_bag"], fusion_output_bag, playback_duration_sec=40
+    recorded_topics = run_fusion_localization_on_sensor_data(
+        args["sensor_bag"], fusion_output_bag, playback_duration_sec=120
     )
-    plot_robot_odometry(fusion_output_bag)
+    plot_robot_odometry(fusion_output_bag, recorded_topics)
     # load + save ekf config 
-    # load launch script path
     # load test configs path
 
 
